ac6223d3e17782e47574e7e1b4fa0a4dc1e06a98
Rian <riansantosjr11@gmail.com>
2022-08-01 18:46:18 -0300

    add resolution 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, num) {
+    let string = '';
+    for (let i = 0; i < num; i++) {
+        string += phrase;
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    return string;
 };
 
 // Do not edit below this line
