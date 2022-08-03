3d1508a06f0e92a1c360f5dd5976754330c4b30d
Rian <riansantosjr11@gmail.com>
2022-08-03 18:48:27 -0300

    add resolution 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fh) {
+  const formula = (fh - 32) * (5/9);
+  let result = Number((formula).toFixed(1));
+  
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(ce) {
+  const formula = (ce * 9/5) + (32);
+  let result = Number((formula).toFixed(1));
+  
+  return result;
 };
 
 // Do not edit below this line
