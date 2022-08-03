e863b1e011e9e135c998308e04cb6a14fc2cd740
Rian <riansantosjr11@gmail.com>
2022-08-03 15:48:49 -0300

    add resolution 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = []; 
+    
+    for (const value of array) {
+        if (!args.includes(value)) {
+            newArray.push(value);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
