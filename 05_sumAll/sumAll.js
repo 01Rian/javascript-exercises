9ab81c22c73c7730b5b7e727711dd9a933cc1463
Rian <riansantosjr11@gmail.com>
2022-08-03 16:50:13 -0300

    add resolution 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let total = 0;
+    
+    if (min > max) {
+        let reverse = min;
+        min = max;
+        max = reverse;
+    }
 
+    if (min < 0 || max < 0) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+    
+    for (let i = min; i <= max; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
