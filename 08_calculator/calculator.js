d3340d9261ccb94e1238f995671ff43102c2709e
Rian <riansantosjr11@gmail.com>
2022-08-20 18:02:11 -0300

    add resolution 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((previousValue, currentValue) =>  previousValue + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((previousValue, currentValue) =>  previousValue * currentValue);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = a;
+  let result = factorial;
+  if (a === 0) return 1;
+  for (let i = 1; i < factorial; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
