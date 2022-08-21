7b1438c652ccd806a75669f166965bb6f48b2bab
Rian <riansantosjr11@gmail.com>
2022-08-21 17:02:15 -0300

    add resolution 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let regex = /[^A-Za-z0-9]/g;
+  let lowRegStr = str.toLowerCase().replace(regex, '');
+  let reverseStr = lowRegStr.split('').reverse().join('');
+  if (reverseStr === lowRegStr) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
