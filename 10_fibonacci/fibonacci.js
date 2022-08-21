59863718015fbf9406d0e25e1a2df84918044ace
Rian <riansantosjr11@gmail.com>
2022-08-21 18:17:04 -0300

    add resolution 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return 'OOPS';
 
+    let fibonacci = [];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    
+    for (let i = 2; i <= num; i++) {
+        fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
+    return fibonacci[fibonacci.length - 1];
 };
 
 // Do not edit below this line
