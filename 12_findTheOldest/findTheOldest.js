33bb651572ed2c80eca5406b732e5de52ea9a90e
Rian <riansantosjr11@gmail.com>
2022-08-22 16:12:29 -0300

    add resolution 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const getAge = (birth, death) => {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    };
+    
+    return arr.reduce((personOldest, currentPerson) => {
+        const oldestAge = getAge(personOldest.yearOfBirth, personOldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : personOldest;
+    })
 };
 
 // Do not edit below this line
